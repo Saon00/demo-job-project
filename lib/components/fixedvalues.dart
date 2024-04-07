00f2b25c0ed735c07355d8e16d1a70606f92dd3d
Saon00 <sikdersaon1@gmail.com>
2024-04-07 22:53:05 +0600

    first commit

---
+++ lib/components/fixedvalues.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color purple = const Color(0xFF7455f7);
+Color white =  Colors.white;
+
+String apiUrl = "https://getlearn-admin.getbuildfirst.com/api/course/details/1";
+
+TextStyle popinsTitle = GoogleFonts.poppins();
---
+++ lib/pages/course_screen.dart
@@ -0,0 +1,491 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:interndemo/components/fixedvalues.dart';
+import 'package:interndemo/model/course_details.dart';
+import 'package:interndemo/model/network_utils.dart';
+import 'package:video_player/video_player.dart';
+import 'package:get/get.dart';
+
+class CourseScreen extends StatefulWidget {
+  const CourseScreen({super.key});
+
+  @override
+  State<CourseScreen> createState() => _CourseScreenState();
+}
+
+class _CourseScreenState extends State<CourseScreen> {
+  late VideoPlayerController controller;
+  CourseDetails courseDetails = CourseDetails();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getAll();
+  }
+
+  Future<void> getAll() async {
+    final response = await NetworkUtils.getMethod(apiUrl);
+    if (response != null) {
+      courseDetails = CourseDetails.fromJson(response);
+      print(courseDetails.data?.requirements?.length);
+    } else {
+      // SnackBar(context, 'Unable to fetch data', true);
+      Get.snackbar("FAILED", "Unable to fetch data",
+          backgroundColor: Colors.red);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: purple,
+      // appbar
+      appBar: AppBar(
+        backgroundColor: purple,
+        title: const Text(
+          "Course Details",
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        leading: InkWell(
+          onTap: () {},
+          child: InkWell(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Image.asset(
+                'assets/icons/menu_bar.png',
+                color: white,
+              ),
+            ),
+          ),
+        ),
+        actions: [
+          InkWell(
+            onTap: () {},
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Image.asset(
+                'assets/icons/shopping_cart.png',
+                color: white,
+              ),
+            ),
+          )
+        ],
+      ),
+
+      // body
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(25), topRight: Radius.circular(25)),
+              color: white),
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+
+                // title text
+                Text(
+                  courseDetails.data?.title.toString() ?? "Title Missing",
+                  style: popinsTitle.copyWith(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+
+                // subtitle
+                Text(
+                  courseDetails.data?.subTitle.toString() ?? "Subtitle Missing",
+                  style: popinsTitle.copyWith(fontSize: 15, color: Colors.grey),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+
+                // ratings
+                Row(
+                  children: [
+                    // rating
+                    Text(
+                      "5.0",
+                      style: popinsTitle.copyWith(
+                          fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+
+                    // stars
+                    const Row(
+                      children: [
+                        Icon(Icons.star, color: Colors.yellow, size: 20),
+                        Icon(Icons.star, color: Colors.yellow, size: 20),
+                        Icon(Icons.star, color: Colors.yellow, size: 20),
+                        Icon(Icons.star, color: Colors.yellow, size: 20),
+                        Icon(Icons.star, color: Colors.yellow, size: 20),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+
+                    // total ratings
+                    Text(
+                      "(25,00)",
+                      style: popinsTitle.copyWith(
+                          fontSize: 15,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+
+                // total students
+                Text(
+                  "9,591 students",
+                  style: popinsTitle.copyWith(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey),
+                ),
+
+                const SizedBox(
+                  height: 15,
+                ),
+
+                // mentor
+                Row(
+                  children: [
+                    Text(
+                      "Mentor",
+                      style: popinsTitle.copyWith(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+
+                    // mentor Name
+                    Text(
+                      "Ashutosh Pawar",
+                      style: popinsTitle.copyWith(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: purple),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+
+                // last update
+                Row(
+                  children: [
+                    Icon(
+                      Icons.calendar_month_outlined,
+                      color: Colors.grey,
+                      size: 20,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "Last update ${courseDetails.data?.updatedAt?.substring(0, 10)}",
+                      style: popinsTitle.copyWith(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+
+                // language
+                Row(
+                  children: [
+                    Icon(
+                      Icons.language_outlined,
+                      color: Colors.grey,
+                      size: 20,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "English",
+                      style: popinsTitle.copyWith(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(
+                  height: 15,
+                ),
+
+                // total students
+                Text(
+                  "BDT ${courseDetails.data?.price}",
+                  style: popinsTitle.copyWith(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey),
+                ),
+
+                // buy now button
+                SizedBox(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: purple,
+                      ),
+                      onPressed: () {},
+                      child: Text("Buy Now",
+                          style: popinsTitle.copyWith(
+                              fontSize: 25,
+                              color: white,
+                              fontWeight: FontWeight.bold)),
+                    )),
+                const SizedBox(
+                  height: 15,
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      height: 55,
+                      child: OutlinedButton(
+                          onPressed: () {}, child: Text('Add to cart')),
+                    ),
+                    SizedBox(
+                      height: 55,
+                      child: OutlinedButton(
+                          onPressed: () {}, child: Text('Add to wishlist')),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+
+                // what you will learn
+                Text(
+                  "What you'll learn",
+                  style: popinsTitle.copyWith(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  height: 120,
+                  child: ListView.separated(
+                      shrinkWrap: true,
+                      // physics: NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return Row(
+                          children: [
+                            Icon(
+                              Icons.circle,
+                              color: Colors.purple,
+                              size: 15,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Expanded(
+                              child: Text(
+                                courseDetails.data?.learningTopic?[index]
+                                        .trim() ??
+                                    "None",
+                                style: popinsTitle.copyWith(
+                                    color: Colors.grey,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            )
+                          ],
+                        );
+                      },
+                      separatorBuilder: (_, __) => SizedBox(
+                            height: 10,
+                          ),
+                      itemCount:
+                          courseDetails.data?.learningTopic?.length ?? 0),
+                ),
+
+                const SizedBox(
+                  height: 15,
+                ),
+
+                Text(
+                  "This course includes",
+                  style: popinsTitle.copyWith(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CourseIncludedWidget(
+                  text: 'Support Files',
+                  icon: Icons.file_copy_outlined,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                CourseIncludedWidget(
+                  text: '34.5 total hours on- demand vedio',
+                  icon: Icons.play_circle_outline_rounded,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                CourseIncludedWidget(
+                  text: '10 Articles',
+                  icon: Icons.article_outlined,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                CourseIncludedWidget(
+                  text: 'Full lifetime access',
+                  icon: Icons.access_time_filled_outlined,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                CourseIncludedWidget(
+                  text: 'Access on mobile, desktop, and TV',
+                  icon: Icons.phone_android_outlined,
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                CourseIncludedWidget(
+                  text: 'Certificate of Completion',
+                  icon: Icons.newspaper_outlined,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                // requierments
+                Text(
+                  "Requirements",
+                  style: popinsTitle.copyWith(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.circle,
+                      color: Colors.purple,
+                      size: 15,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Expanded(
+                      child: Text(
+                        courseDetails.data?.requirements ?? "None",
+                        style: popinsTitle.copyWith(
+                            color: Colors.grey,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                // descriptions
+                Text(
+                  "Descriptions",
+                  style: popinsTitle.copyWith(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  courseDetails.data?.description.toString() ??
+                      "Description Missing",
+                  style: popinsTitle.copyWith(
+                      fontSize: 15,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CourseIncludedWidget extends StatelessWidget {
+  final String text;
+  final IconData icon;
+
+  const CourseIncludedWidget({
+    super.key,
+    required this.text,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          color: Colors.purple[400],
+          size: 20,
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        Expanded(
+          child: Text(
+            text,
+            style: popinsTitle.copyWith(
+              color: Colors.black,
+              fontSize: 15,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
